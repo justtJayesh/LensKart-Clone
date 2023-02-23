47c810fb51d08db8833838f06c907732b31dbba2
Jayesh Mate <jayesh23mate@gmail.com>
2023-02-23 14:52:50 +0530

    push testing error alt

---
+++ src/Pages/Products.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Heading } from '@chakra-ui/react'
+
+
+
+const Products = () => {
+    return (
+        <div>
+            <Heading>Products</Heading>
+        </div>
+    );
+
+}
+
+export default Products
